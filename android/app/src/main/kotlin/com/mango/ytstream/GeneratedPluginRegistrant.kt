028d53545377aea424e02d79ba4455d3609b836e
AlgoTradingLive <smkinpune@gmail.com>
2026-06-21 01:11:12 +0530

    Fix android/app/src/main/kotlin/com/mango/ytstream/GeneratedPluginRegistrant.kt

---
+++ android/app/src/main/kotlin/com/mango/ytstream/GeneratedPluginRegistrant.kt
@@ -0,0 +1,9 @@
+package com.mango.ytstream
+
+import io.flutter.embedding.engine.FlutterEngine
+
+object GeneratedPluginRegistrant {
+    fun registerWith(flutterEngine: FlutterEngine) {
+        // plugins registered here
+    }
+}
